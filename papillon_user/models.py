de555ff94db55fd9b7737446d3608ebc19bafe72
Théophile Bastian <contact@tobast.fr>
2018-03-01 17:34:09 +0100

    Add readable __str__ for papillon_user

--- papillon_user/models.py
+++ papillon_user/models.py
@@ -9,6 +9,9 @@ class PapillonUser(models.Model):
                                        on_delete=models.CASCADE,
                                        primary_key=True)
 
+    def __str__(self):
+        return str(self.django_user)
+
 
 def get_papillon_user(user):
     if not user.is_authenticated:
