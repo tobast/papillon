034ba1edb8731b0a47bcb72e457c1e65b452c83a
Théophile Bastian <contact@tobast.fr>
2018-03-01 11:25:27 +0100

    Init Django project

---
+++ papillon/settings_local.default.py
@@ -0,0 +1,30 @@
+""" Local settings """
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PUBLIC_DIR = os.path.join(BASE_DIR, 'public')
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '@-=k62(!6%kj7f$%i*n)u=7e(+ncjx^&ia=_ah@0v+dur1q4k^'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Time zone
+TIME_ZONE = 'UTC'
+
+# Static files' directory
+STATIC_DIR = os.path.join(PUBLIC_DIR, 'static')
